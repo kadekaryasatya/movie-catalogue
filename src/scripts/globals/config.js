b4d5b8c7e4a0383f38e7c9035d8f8bdecdca25b1
dekarya123 <arya.satyad123@gmail.com>
2022-11-12 00:52:23 +0800

    Service worker

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,8 +1,9 @@
 const CONFIG = {
-  KEY: "159587378b9cc4023c8fe223f9fba611",
+  KEY: "YOUR_API_KEY",
   BASE_URL: "https://api.themoviedb.org/3/",
   BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
   DEFAULT_LANGUAGE: "en-us",
+  CACHE_NAME: "MovieCatalogue-V1",
 };
 
 export default CONFIG;
